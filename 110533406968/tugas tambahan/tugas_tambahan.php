21d064d3448fcf87d83bf9725d233ce7952b3acd
RahmaDR <anjar.dr22@gmail.com>
2013-10-16 22:07:06 +0700

    tugas tambahan
    tugas tambahan

---
+++ 110533406968/tugas tambahan/tugas_tambahan.php
@@ -0,0 +1,94 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+	<title>KALKULATOR SEDERHANA</title>
+	<style type = "text/css">
+	<!--
+	body{
+	background-color : #8EDAF0;
+	} 
+	.login{
+	margin : 180px 500px 180px 500px;
+	background-color : #999999;
+	width : 250px;
+	height : 250px;
+	padding : 10px 10px 10px 10px;
+	border : 3px solid blue;
+	}
+	-->
+	</style>
+</head>
+
+<body>
+<div class = "login">
+<h1 align = 'center'>KALKULATOR SEDERHANA</h1>
+<hr/ color = 'blue'>
+<form action = "<?php $_SERVER['PHP_SELF'];?>" method = "get">
+
+	<input type = 'number' name = 'a' value = "<?php echo isset ($_GET['a']) ? $_GET['a'] : ''; ?>">
+
+	<select name = "operator">
+	<option value = "+" <?php 
+	if (@$_GET['operator'] == '+') {
+	echo 'selected = "selected"';
+	}
+	?>> +
+	<option value = "-" <?php 
+	if (@$_GET['operator'] == '-') {
+	echo 'selected = "selected"';
+	}
+	?>> -
+	<option value = "/"
+	<?php 
+	if (@$_GET['operator'] == '/') {
+	echo 'selected = "selected"';
+	}
+	?>> /
+	<option value = "*"
+	<?php 
+	if (@$_GET['operator'] == '*') {
+	echo 'selected = "selected"';
+	}
+	?>> *
+	</select>
+
+	<input type = 'number' name = 'b' value = "<?php echo isset ($_GET['b']) ? $_GET['b'] : ''; ?>" >
+	<input type = "submit" value = "="/>
+
+</form>
+
+<form>
+<?php 
+$a = $_GET['a'];
+$b = $_GET['b'];
+?>
+<?php
+if (isset ($_GET['operator'])) {
+	if (@$_GET['operator'] == '+') {
+	$c = $a + $b;
+	}
+	elseif (@$_GET['operator'] == '-') {
+	$c = $a - $b;
+	}
+	elseif (@$_GET['operator'] == '/') {
+	$c = $a / $b;
+	}
+	elseif (@$_GET['operator'] == '*') {
+	$c = $a * $b;
+	}
+	}?>
+
+	<input type = 'text' name = 'b' value = '<?php echo $c;?>'>
+
+<?php	
+if (isset($_GET["a"]) && isset($_GET["b"]) && ($_GET["a"] =="") && ($_GET["b"] == "")){
+?><script language = "JavaScript">
+alert("belum ada angka yang akan dioperasikan")
+document.location='tugas_tambahan.php'</script><?php
+} ?>
+
+</form>
+
+</div>
+</body>
+</html>
